f1eda82794a15b475b927b9ab4a0f396700628fc
rotemga <rotem.ge100@gmail.com>
2016-03-14 13:54:55 +0200

    Update House.h

--- House.h
+++ House.h
@@ -1,13 +1,16 @@
-#pragma once
-
 
 #ifndef HOUSE_H_
 #define HOUSE_H_
-
+#include <vector>
 #include <string>
 #include <iostream>
 
 using namespace std;
+
+typedef struct point {
+	int x;
+	int y;
+} Point;
 class House {
 	string shortDes;
 	string longDes;
@@ -18,8 +21,34 @@ class House {
 
 public:
 	House();
+	int currentValue(int x, int y) const;
+	bool findDockingStation(Point&) const;
+	char& findDirtLevel(int x, int y) const;
 	virtual ~House();
 	void fillHouseData(string& filename);
+	void putWallsOnSides();
+	int sumDirt() const;
+
+	int getC() const {
+		return C;
+	}
+
+	void setC(int c) {
+		C = c;
+	}
+
+	string* getMatrix() const {
+		return matrix;
+	}
+
+
+	int getR() const {
+		return R;
+	}
+
+	void setR(int r) {
+		R = r;
+	}
 };
 
 #endif /* HOUSE_H_ */
