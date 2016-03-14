eb33e1890bddb19454017f670caa9677fb6045f5
rotemga <rotem.ge100@gmail.com>
2016-03-14 09:25:04 +0200

    Update OurSensor.h

--- OurSensor.h
+++ OurSensor.h
@@ -1,19 +1,33 @@
 #pragma once
 #include "AbstractSensor.h"
+#include "OurSensor.h"
+#include "House.h"
+#include <string>
+#include <map>
+#include <stdlib.h> 
+
+/*struct Position {
+	int row;
+	int col;
+};*/
 class OurSensor : public AbstractSensor
 {
-	Position* sensorPos;
+	//Position* sensorPos;
 	SensorInformation* sensorInfo;
 public:
 	OurSensor();
+
+	OurSensor(SensorInformation *docking) : sensorInfo(docking){
+
+	}
 	~OurSensor();
 	OurSensor(const OurSensor&);//copy c'tor
 	OurSensor& operator=(const OurSensor&);// '=' operator
-	OurSensor& operator=(const AbstractSensor&);// '=' operator
-
+	char* ToString(const Direction direct);
+	bool directionIsLegal(const Direction direct);//return true if the direction is legal
 	virtual SensorInformation sense() const;
-	virtual SensorInformation setInfo(SensorInformation& Info);
-	void OurSensor::setPos(Position& pos);
+	virtual void setInfo(SensorInformation& Info);
+	//void setPos(Position& pos);
 };
 
 
