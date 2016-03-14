417c7b347128289e00dd7bee114623c1c4b3f818
rotemga <rotem.ge100@gmail.com>
2016-03-14 10:01:24 +0200

    Update AbstractSensor.cpp

--- AbstractSensor.cpp
+++ AbstractSensor.cpp
@@ -9,31 +9,3 @@ AbstractSensor::AbstractSensor()
 AbstractSensor::~AbstractSensor()
 {
 }
-
-char* AbstractSensor::ToString(const Direction direct){
-	switch (direct)
-	{
-	case Direction::East:		return "East";
-	case Direction::West:		return "West";
-	case Direction::South:		return "South";
-	case Direction::North:		return "North";
-	case Direction::Stay:		return "Stay";
-	default:					return "Unknown";
-	}
-}
-
-//return true if the direction is legal.
-//i.e. the direction is not lead to wall, or if the direction is stay, there is dust in the current location.
-bool AbstractSensor::directionIsLegal(const Direction direct){
-
-	switch (direct)
-	{
-	case Direction::East:		return !(sensorInfo->isWall[0]);
-	case Direction::West:		return !(sensorInfo->isWall[1]);
-	case Direction::South:		return !(sensorInfo->isWall[2]);
-	case Direction::North:		return !(sensorInfo->isWall[3]);
-	case Direction::Stay:		return (sensorInfo->dirtLevel > 0);
-	default:					return false;
-	}
-
-}
