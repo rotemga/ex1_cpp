d6aa65caa2f6b2ed73e2daba2d164c2c0ba2e297
rotemga <rotem.ge100@gmail.com>
2016-03-14 10:00:38 +0200

    Update AbstractSensor.h

--- AbstractSensor.h
+++ AbstractSensor.h
@@ -7,28 +7,13 @@ using namespace std;
 enum class Direction { East, West, South, North, Stay };
 struct SensorInformation {
 	int dirtLevel;
-	bool isWall[4]; //index 0 is east, 1 is west, 2 is south, 3 is north
+	bool isWall[4];//index 0 is east, 1 is west, 2 is south, 3 is north
 };
+class AbstractSensor {
 
-struct Position {
-	int row;
-	int col;
-};
-
-
-class AbstractSensor
-{
-	Position* sensorPos;
-	SensorInformation* sensorInfo;
 public:
 	AbstractSensor();
 	~AbstractSensor();
-	AbstractSensor(const AbstractSensor&);//copy c'tor
-	AbstractSensor& operator=(const AbstractSensor&);// '=' operator
 	// returns the sensor's information of the current location of the robot
 	virtual SensorInformation sense() const = 0;
-	char* ToString(const Direction direct);
-	bool directionIsLegal(const Direction direct);//return true if the direction is legal
 };
-
-
