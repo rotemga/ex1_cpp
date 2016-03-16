494f21d870b9ebdb7ae239a9386b6dbba9704392
rotemga <rotem.ge100@gmail.com>
2016-03-16 13:06:47 +0200

    Update AbstractAlgorithm.h

--- AbstractAlgorithm.h
+++ AbstractAlgorithm.h
@@ -1,25 +1,30 @@
-#pragma once
+#ifndef __ABSTRACT_ALGORITHM__H_
+#define __ABSTRACT_ALGORITHM__H_
 
-#include "AbstractSensor.h"
-////#include <string>
 #include <map>
-
-
+#include "Direction.h"
 using namespace std;
 
-class AbstractAlgorithm {
+#include "AbstractSensor.h"
 
+class AbstractAlgorithm
+{
 public:
-	AbstractAlgorithm();
-	~AbstractAlgorithm();
-	// setSensor is called once when the Algorithm is initialized
+	virtual ~AbstractAlgorithm() {}
+
+	// setSensor is called once when the Algorithm is initialized 
 	virtual void setSensor(const AbstractSensor& sensor) = 0;
-	// setConfiguration is called once when the Algorithm is initialized - see below
+
+	// setConfiguration is called once when the Algorithm is initialized - see below 
 	virtual void setConfiguration(map<string, int> config) = 0;
-	// step is called by the simulation for each time unit
+
+	// step is called by the simulation for each time unit 
 	virtual Direction step() = 0;
-	// this method is called by the simulation either when there is a winner or
-	// when steps == MaxSteps - MaxStepsAfterWinner
-	// parameter stepsTillFinishing == MaxStepsAfterWinner
+
+	// this method is called by the simulation either when there is a winner or 
+	// when steps == MaxSteps - MaxStepsAfterWinner 
+	// parameter stepsTillFinishing == MaxStepsAfterWinner 
 	virtual void aboutToFinish(int stepsTillFinishing) = 0;
 };
+
+#endif //__ABSTRACT_ALGORITHM__H_
