2b29a12ffd1b115132ac9c9e337a002958de9427
rotemga <rotem.ge100@gmail.com>
2016-03-16 19:09:34 +0200

    Update Robot.cpp

--- Robot.cpp
+++ Robot.cpp
@@ -1,56 +1,75 @@
 #include "Robot.h"
 
-
-Robot::Robot(House *house, AbstractAlgorithm *algo, Point* docking, Battery* battery) : house(house), algo(algo), docking(docking), battery(battery)
-{
+Robot::Robot(House *house, AbstractAlgorithm *algo, Point* docking,
+	Battery* battery) :
+	house(house), algo(algo), position(docking), battery(battery) {
 	sensor = new OurSensor(house, docking);
-	score = new Score();
+	score = *new Score();
+	score.setSumDirtInHouse(house->sumDirt());
 	canRun = true;
 }
 
-
-Robot::~Robot()
-{
+Robot::~Robot() {
 	//sensor = delete;
 }
 
-
-void Robot::runRobot(){
-	while (canRun){
-
+void Robot::runRobot() {
+	if (canRun) {
+		updateHouse(*position);
+		updateBattery(*position, *battery);
+		Direction direction = algo->step(); //should check if direction is legal
+		position->move(direction);
+		sensor->getInfoFromPoint(house, position);
+		score.setNumSteps(score.getNumSteps() + 1);
 	}
 }
 
-void Robot::updateHouse(int xlocation, int ylocation) {
-	if (house->currentValue(xlocation, ylocation) != '0')
-		house->getMatrix()[xlocation][ylocation]--;
-}
-void Robot::updateBattery(int x, int y, int& batteryState) {
-	/*if (house->getMatrix()[x][y] != 'D')
-		batteryState -= config.at("BatteryConsumptionRate");
-	else {
-		int newLevel = batteryState + config.at("BatteryRachargeRate");
-		batteryState =
-			(newLevel > config.at("BatteryCapacity")) ?
-			config.at("BatteryCapacity") : newLevel;
-	}*/
-}
-void Robot::updatePointByDirection(Point& point, Direction direction) {
-	switch (direction) {
-	case Direction::East:
-		point.x++;
+void Robot::updateHouse(const Point& point) {
+	switch (house->findPointState(point)) {
+	case DIRTY:
+		house->cleanDirtyPoint(point);
 		break;
-	case Direction::West:
-		point.x--;
-		break;
-	case Direction::North:
-		point.y++;
-		break;
-	case Direction::South:
-		point.y--;
-		break;
-	case Direction::Stay:
-		break;
-	default:;
+	deafult:;
 	}
 }
+void Robot::updateBattery(Point& point, Battery& battery) {
+	switch (house->findPointState(point)) {
+	case DOCKING:
+		battery.recharge();
+		break;
+	default:
+		battery.reduce();
+	}
+}
+
+bool Robot::isHouseClean() const {
+	return house->isClean();
+}
+bool Robot::areWeInDocking() const {
+	return house->findPointState(*position) == DOCKING;
+}
+
+int Robot::DirtCollected(){
+	return (score.getSumDirtInHouse() - house->sumDirt());
+}
+
+//void Robot::updatePointByDirection(Point& point, Direction direction) {
+//	switch (direction) {
+//	case Direction::East:
+//		point.x++;
+//		break;
+//	case Direction::West:
+//		point.x--;
+//		break;
+//	case Direction::North:
+//		point.y++;
+//		break;
+//	case Direction::South:
+//		point.y--;
+//		break;
+//	case Direction::Stay:
+//		break;
+//	default:;
+//	}
+//}
+
