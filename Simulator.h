48b61b9aba880125a1d932d09f03c8ed435ee87c
rotemga <rotem.ge100@gmail.com>
2016-03-16 19:08:50 +0200

    Update Simulator.h

--- Simulator.h
+++ Simulator.h
@@ -12,6 +12,7 @@
 #include "OurSensor.h"
 #include "Robot.h"
 #include "Battery.h"
+#include "Point.h"
 using namespace std;
 
 class Simulator {
@@ -24,13 +25,13 @@ class Simulator {
 public:
 	void run();
 	void fillHouses(const string& location);
-	void Simulator::setInputHouses(vector<House*> input);
-	void Simulator::setInputConfig(map<string, int> input_config);
-	void Simulator::setInputAlgo(vector <AbstractAlgorithm*> input_algorithms);
+	void setInputHouses(vector<House*> input);
+	void setInputConfig(map<string, int> input_config);
+	void setInputAlgo(vector <AbstractAlgorithm*> input_algorithms);
+	void createScore(int num_steps, int pos_in_comeptition, bool is_back_docking, int dirt_collected, Score *score);
 	Simulator();
 	virtual ~Simulator();
 
 };
 
 #endif /* SIMULATOR_H_ */
-
