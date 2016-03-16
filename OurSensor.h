141e383951248a51ab7d9ddafc90c3cf6c9276a6
rotemga <rotem.ge100@gmail.com>
2016-03-16 13:10:29 +0200

    Update OurSensor.h

--- OurSensor.h
+++ OurSensor.h
@@ -1,22 +1,30 @@
 #pragma once
+
+#ifndef OURSENSOR_H_
+#define OURSENSOR_H_
+
 #include "AbstractSensor.h"
+#include "House.h"
 
 
 class OurSensor : public AbstractSensor
 {
 	SensorInformation* sensorInfo;
+	House *thisHouse;
+	Point *currPoint;
 public:
-	OurSensor();
 
-	OurSensor(SensorInformation *docking) : sensorInfo(docking){
+	/*OurSensor(SensorInformation *docking) : sensorInfo(docking){
 
-	}
+	}*/
+	OurSensor(House *house, Point* point);
 	~OurSensor();
 	OurSensor(const OurSensor&);//copy c'tor
 	OurSensor& operator=(const OurSensor&);// '=' operator
 	virtual SensorInformation sense() const override;
 	virtual void setInfo(SensorInformation& Info);
+	SensorInformation OurSensor::getInfoFromPoint();
 };
 
 
-
+#endif /* OURSENSOR_H_ */
